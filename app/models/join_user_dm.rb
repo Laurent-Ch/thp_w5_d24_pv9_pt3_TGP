8fe8fe67fa3cece41eaff1683fd696d9b2ce7951
Laurent-Ch <chamouleau.laurent@gmail.com>
2021-10-28 17:46:12 +0200

    tables written

---
+++ app/models/join_user_dm.rb
@@ -0,0 +1,2 @@
+class JoinUserDm < ApplicationRecord
+end
---
+++ test/models/join_user_dm_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class JoinUserDmTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
