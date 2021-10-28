09cde9a160b9a20ec5699f63a8f43e28111aa8f0
Laurent-Ch <chamouleau.laurent@gmail.com>
2021-10-29 01:07:50 +0200

    a better user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   belongs_to :city
   
-  has_many :sent_messages, foreign_key: 'sender_id', class_name: "PrivateMessage"
+  has_many :sent_messages, foreign_key: 'sender_id', class_name: "Dm"
   
   has_many :join_user_dms  
-  has_many :received_messages, foreign_key: 'recipient_id', class_name: "PrivateMessage", through: :join_user_dms
+  has_many :received_messages, foreign_key: 'recipient_id', class_name: "Dm", through: :join_user_dms
 
   has_many :join_tag_gossips
   has_many :gossips, through: :join_tag_gossips
